dbb8ebf4209be325d7a286536d63e548c7e02966
StarHeartY <yirui.pre@gmail.com>
2026-08-30 19:59:12 +0800

    test2

--- src/data/locales/zh.ts
+++ src/data/locales/zh.ts
@@ -81,7 +81,7 @@ const zh: SiteContent = {
   download: {
     title: '获取 CalculatorX',
     steps: [],
-    note: '也可通过开源分发渠道获取安装包。CalculatorX 基于 GPL 3.0 协议开源，源代码完全公开，欢迎审计与贡献。',
+    note: '也可通过开源渠道获取安装包。CalculatorX 基于 GPL 3.0 协议开源，源代码完全公开，欢迎审计与贡献。',
     buttonLabel: 'AppGallery',
     buttonUrl: 'https://appgallery.huawei.com/app/detail?id=com.startyi.calcx',
     githubButton: '访问 GitHub',
